e88d96591dd58c580bd4a8911e5906d32d5818b3
Zyphra <zyphra.dev@gmail.com>
2026-05-25 15:20:10 +0500

    Massive updates

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -22,7 +22,7 @@ const ProductGrid: React.FC = () => {
       opacity: 1,
       y: 0,
       scale: 1,
-      transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] },
+      transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] as const },
     },
   };
 
